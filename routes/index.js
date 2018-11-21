6f62f8e01fc7938c4bf27356566b7c6b7eddda47
Adrian Mustea <mustea.adrian95@gmail.com>
2018-11-21 19:37:23 +0200

    added some btns

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config()
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+
+const msg = (data) => {
+  let newData = JSON.parse(data);
+  let email = newData.email
+  let name = newData.name
+  let description = newData.description
+  let phone = newData.phone
+  return{
+  to: 'mustea.adrian95@gmail.com',
+  from: 'viralpixel@virelpixel.studio',
+  subject: `${name} is interested`,
+  text: `${name} is interested in our services.His project: ${description} ; his contacts: ${email}, ${phone} `
+  }
+};
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'ViralPixel - digital art' });
 });
+
+// router.post('/send/contact/:body', function(req, res, next) {
+//   sgMail.send(msg(req.params.body));
+//   res.send('');
+//   res.status(200)
+// })
 
 module.exports = router;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,34 +4,82 @@ import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import Icon from '@material-ui/core/Icon'
 import Button from '@material-ui/core/Button'
-import SaveIcon from '@material-ui/icons/Save';
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 export default class Contact extends Component {
-  render() {
+    constructor() {
+        super()
+        this.state = {
+            name: '',
+            email: '',
+            description: '',
+            phone: '',
+            status: false
+        }
+    }
+
+    sendMessage = () => {
+        const name = this.state.name;
+        const email = this.state.email;
+        const description = this.state.description;
+        const phone = this.state.phone;
+        const body = JSON.stringify({name, email, description, phone});
+        fetch(`/send/contact/${body}`, {
+            method: 'POST',
+            body: body
+        }).then(() => {
+                this.setState({
+                    name: '',
+                    email: '',
+                    description: '',
+                    phone: '',
+                    status: true
+                })
+                console.log(this.state)
+            }).catch(error => console.log(data, error));
+    };
+
+
+  render() {    
     return (
         <FormControl id = "contact-form-react">
             <TextField 
+                onChange = {ev => this.setState({name: ev.target.value})}
                 required
+                value = {this.state.name}
                 placeholder = "Your name"
-                
             >
             </TextField>
-            <TextField 
+            <TextField
+                onChange = {ev => this.setState({email: ev.target.value})}
                 required
+                value = {this.state.email}
                 placeholder = "Your email"
+                
+            >
+            
+            </TextField>
+            <TextField
+                onChange = {ev => this.setState({phone: ev.target.value})}
+                required
+                placeholder = "Your phone"
+                value = {this.state.phone}
                 
             >
             </TextField>
             <TextField
                 //required
+                onChange = {ev => this.setState({description: ev.target.value})}
                 placeholder = "Tell us about your project"
                 multiline
+                value = {this.state.description}
                 margin = "normal"
+                
             >
             </TextField>
-            <Button id = "sendButton" variant="contained">
-                Send
-                <Icon>send</Icon>
-            </Button>
+                <Button style = {{backgroundColor: this.state.status === true? "#1fe5bd" : "rgb(252, 0, 114)"}}onClick = {this.sendMessage} id = "sendButton" variant="contained" text = {this.state.status === true? 'Send' : 'Thank you!'}>
+                    {this.state.status === false? 'Send' : 'Thank you!'}
+                    <Icon style = {{marginLeft: "0.5em"}}>{this.state.status === false? 'send' : 'done_all'}</Icon>
+                </Button>
         </FormControl>
     )
   }
